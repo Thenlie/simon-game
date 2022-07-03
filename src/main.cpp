0c54368415f1a658d85c6d7e1f585e59d4dcfbe1
Leithen <leithen113@gmail.com>
2022-07-02 23:03:25 -0600

    Refactored code for breadboard without transistors

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
-#define RED_LED 2 // Red LED
-#define YELLOW_LED 3 // Yellow LED
-#define GREEN_LED 4 // Green LED
-#define BLUE_LED 5 // Blue LED
-#define INPUT_ONE 8
-#define INPUT_TWO 9
-#define INPUT_THREE 10
-#define INPUT_FOUR 11
+#define RED_LED 8 // Red LED
+#define YELLOW_LED 9 // Yellow LED
+#define GREEN_LED 10 // Green LED
+#define BLUE_LED 11 // Blue LED
+#define INPUT_ONE 3
+#define INPUT_TWO 4
+#define INPUT_THREE 5
+#define INPUT_FOUR 6
 
 int val = 0;
 
@@ -25,51 +25,28 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
   val = digitalRead(INPUT_ONE);
   if(val == HIGH) {
+    digitalWrite(RED_LED, HIGH);
     Serial.println("HIGH");
   }
-  if(val == LOW) {
-    Serial.println("LOW");
-  }
-  delay(1000);
   val = digitalRead(INPUT_TWO);
   if(val == HIGH) {
+    digitalWrite(YELLOW_LED, HIGH);
     Serial.println("HIGH");
   }
-  if(val == LOW) {
-    Serial.println("LOW");
-  }
-  delay(1000);
   val = digitalRead(INPUT_THREE);
   if(val == HIGH) {
+    digitalWrite(GREEN_LED, HIGH);
     Serial.println("HIGH");
   }
-  if(val == LOW) {
-    Serial.println("LOW");
-  }
-  delay(1000);
   val = digitalRead(INPUT_FOUR);
   if(val == HIGH) {
+    digitalWrite(BLUE_LED, HIGH);
     Serial.println("HIGH");
   }
-  if(val == LOW) {
-    Serial.println("LOW");
-  }
-  delay(1000);
-  
-  
-  // digitalWrite(RED_LED, HIGH);
-  // delay(1000);
-  // digitalWrite(RED_LED, LOW);
-  // digitalWrite(YELLOW_LED, HIGH);
-  // delay(1000);
-  // digitalWrite(YELLOW_LED, LOW);
-  // digitalWrite(GREEN_LED, HIGH);
-  // delay(1000);
-  // digitalWrite(GREEN_LED, LOW);
-  // digitalWrite(BLUE_LED, HIGH);
-  // delay(1000);
-  // digitalWrite(BLUE_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
 }
