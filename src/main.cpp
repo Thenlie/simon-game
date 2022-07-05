b41aa936c8f65d315e93b29a1d5cdb6f65b935c6
Leithen <leithen113@gmail.com>
2022-07-04 21:13:10 -0600

    Increased indentation size

--- src/main.cpp
+++ src/main.cpp
@@ -16,125 +16,127 @@ bool running = false; // game state
 
 // run once on setup
 void setup() {
-  pinMode(RED_LED, OUTPUT);
-  pinMode(YELLOW_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
-  pinMode(INPUT_ONE, INPUT);
-  pinMode(INPUT_TWO, INPUT);
-  pinMode(INPUT_THREE, INPUT);
-  pinMode(INPUT_FOUR, INPUT);
-  for (int i = 0; i < 100; i++) {
-    patternArr[i] = random(4) + 1; 
-  }
-  Serial.begin(9600);
+    pinMode(RED_LED, OUTPUT);
+    pinMode(YELLOW_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
+    pinMode(INPUT_ONE, INPUT);
+    pinMode(INPUT_TWO, INPUT);
+    pinMode(INPUT_THREE, INPUT);
+    pinMode(INPUT_FOUR, INPUT);
+    for (int i = 0; i < 100; i++) {
+        patternArr[i] = random(4) + 1; 
+    }
+    Serial.begin(9600);
 }
 
 void startGameLights() {
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(RED_LED, HIGH);
-    delay(75);
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(YELLOW_LED, HIGH);
-    delay(75);
-    digitalWrite(YELLOW_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
-    delay(75);
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(BLUE_LED, HIGH);
-    delay(75);
-    digitalWrite(BLUE_LED, LOW);
-  }
+    for (int i = 0; i < 5; i++) {
+        digitalWrite(RED_LED, HIGH);
+        delay(75);
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(YELLOW_LED, HIGH);
+        delay(75);
+        digitalWrite(YELLOW_LED, LOW);
+        digitalWrite(GREEN_LED, HIGH);
+        delay(75);
+        digitalWrite(GREEN_LED, LOW);
+        digitalWrite(BLUE_LED, HIGH);
+        delay(75);
+        digitalWrite(BLUE_LED, LOW);
+    }
 }
 
 void displayPattern() {
-  for (int i = 0; i < level; i++) {
-    switch (patternArr[i]) {
-      case 1:
-        digitalWrite(RED_LED, HIGH);
-        delay(100);
-        digitalWrite(RED_LED, LOW);
-        break;
-      case 2:
-        digitalWrite(YELLOW_LED, HIGH);
-        delay(100);
-        digitalWrite(YELLOW_LED, LOW);
-        break;
-      case 3:
-        digitalWrite(GREEN_LED, HIGH);
-        delay(100);
-        digitalWrite(GREEN_LED, LOW);
-        break;
-      case 4:
-        digitalWrite(BLUE_LED, HIGH);
-        delay(100);
-        digitalWrite(BLUE_LED, LOW);
-        break;
-    }
+    for (int i = 0; i < level; i++) {
+        switch (patternArr[i]) {
+            case 1:
+                digitalWrite(RED_LED, HIGH);
+                delay(100);
+                digitalWrite(RED_LED, LOW);
+                break;
+            case 2:
+                digitalWrite(YELLOW_LED, HIGH);
+                delay(100);
+                digitalWrite(YELLOW_LED, LOW);
+                break;
+            case 3:
+                digitalWrite(GREEN_LED, HIGH);
+                delay(100);
+                digitalWrite(GREEN_LED, LOW);
+                break;
+            case 4:
+                digitalWrite(BLUE_LED, HIGH);
+                delay(100);
+                digitalWrite(BLUE_LED, LOW);
+                break;
+        }
     delay(50);
   }
 }
 
 void startGame() {
-  running = true;
-  startGameLights();
-  // countdown flashes
-  for (int i = 0; i < 3; i++) {
-    // all LEDs on
-    digitalWrite(RED_LED, HIGH);
-    digitalWrite(YELLOW_LED, HIGH);
-    digitalWrite(GREEN_LED, HIGH);
-    digitalWrite(BLUE_LED, HIGH);
-    delay(500);
-    // all LEDs off
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(YELLOW_LED, LOW);
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(BLUE_LED, LOW);
-    delay(500);
-  }
-  displayPattern();
+    running = true;
+    startGameLights();
+    // countdown flashes
+    for (int i = 0; i < 3; i++) {
+        // all LEDs on
+        digitalWrite(RED_LED, HIGH);
+        digitalWrite(YELLOW_LED, HIGH);
+        digitalWrite(GREEN_LED, HIGH);
+        digitalWrite(BLUE_LED, HIGH);
+        delay(500);
+        // all LEDs off
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(YELLOW_LED, LOW);
+        digitalWrite(GREEN_LED, LOW);
+        digitalWrite(BLUE_LED, LOW);
+        delay(500);
+    }
+    displayPattern();
 }
 
 // main loop
 void loop() {
-  // check for button presses, turn on associated LED
-  val = digitalRead(INPUT_ONE);
-  if (val == HIGH) {
-    btnCount++;
-    digitalWrite(RED_LED, HIGH);
-  }
-  val = digitalRead(INPUT_TWO);
-  if (val == HIGH) {
-    btnCount++;
-    digitalWrite(YELLOW_LED, HIGH);
-  }
-  val = digitalRead(INPUT_THREE);
-  if (val == HIGH) {
-    btnCount++;
-    digitalWrite(GREEN_LED, HIGH);
-  }
-  val = digitalRead(INPUT_FOUR);
-  if (val == HIGH) {
-    btnCount++;
-    digitalWrite(BLUE_LED, HIGH);
-  }
-  // check if all buttons were pushed at once
-  if (btnCount == 4) {
-    Serial.println("ALL PRESSED!");
-    delay(500);
+    if (!running) {
+        // check for button presses, turn on associated LED
+        val = digitalRead(INPUT_ONE);
+        if (val == HIGH) {
+            btnCount++;
+            digitalWrite(RED_LED, HIGH);
+        }
+        val = digitalRead(INPUT_TWO);
+        if (val == HIGH) {
+            btnCount++;
+            digitalWrite(YELLOW_LED, HIGH);
+        }
+        val = digitalRead(INPUT_THREE);
+        if (val == HIGH) {
+            btnCount++;
+            digitalWrite(GREEN_LED, HIGH);
+        }
+        val = digitalRead(INPUT_FOUR);
+        if (val == HIGH) {
+            btnCount++;
+            digitalWrite(BLUE_LED, HIGH);
+        }
+        // check if all buttons were pushed at once
+        if (btnCount == 4) {
+            Serial.println("ALL PRESSED!");
+            delay(500);
+            // all LEDs off
+            digitalWrite(RED_LED, LOW);
+            digitalWrite(YELLOW_LED, LOW);
+            digitalWrite(GREEN_LED, LOW);
+            digitalWrite(BLUE_LED, LOW);
+
+            startGame();
+        }
+    }
+    btnCount = 0;
     // all LEDs off
     digitalWrite(RED_LED, LOW);
     digitalWrite(YELLOW_LED, LOW);
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(BLUE_LED, LOW);
-
-    startGame();
-  }
-  btnCount = 0;
-  // all LEDs off
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(YELLOW_LED, LOW);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BLUE_LED, LOW);
 }
